491a43bc27719ecdd3aed481ddb4f8fc901d736e
Nithin-Holla <nithin.holla7@gmail.com>
2020-01-08 17:19:18 +0100

    More efficient toy dataset

--- datasets/toy_dataset.py
+++ datasets/toy_dataset.py
@@ -7,16 +7,16 @@ class ToyDataset(data.Dataset):
 
     def __init__(self, n_examples):
         self.n_examples = n_examples
+        self.r = np.random.choice([0, 1], size=(n_examples, 1))
+        self.v = np.random.randn(n_examples, 1) + self.r
+        self.u = np.random.randn(n_examples, 1) + self.v
+        self.w = np.random.randn(n_examples, 1) + self.v
+        self.x = torch.tensor(np.hstack([self.r, self.u]))
+        self.y = torch.tensor(self.w > 0).float()
+        self.z = torch.tensor(self.r).float()
 
     def __len__(self):
         return self.n_examples
 
     def __getitem__(self, index):
-        r = np.random.choice([0, 1])
-        v = np.random.randn() + r
-        u = np.random.randn() + v
-        w = np.random.randn() + v
-        x = torch.tensor([r, u])
-        y = torch.tensor([float(w > 0)])
-        z = torch.tensor([float(r)])
-        return x, y, z
+        return self.x[index], self.y[index], self.z[index]
