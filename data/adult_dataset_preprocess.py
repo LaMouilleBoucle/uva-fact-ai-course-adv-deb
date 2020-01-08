fc80f9c4483cf3ef4a6aec52aff75e7e2dca56df
Azamat Omuraliev <azamat.omu@gmail.com>
2020-01-08 16:28:05 +0100

    initialized data preprocessing script for UCI Adult

---
+++ data/adult_dataset_preprocess.py
@@ -0,0 +1,11 @@
+from torch.utils.data import Dataset
+
+class AdultUCI(Dataset):
+    def __init__(self):
+
+    def __len__(self):
+
+    def __getitem__(self, idx):
+
+if __name__ == '__main__':
+    dataset = AdultUCI()
