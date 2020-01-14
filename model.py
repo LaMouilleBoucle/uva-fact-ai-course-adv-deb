0fbd9678f99783d3529000ac2f03038a7e8ec3e0
LaMouilleBoucle <leilatalha@gmail.com>
2020-01-14 11:52:04 +0100

    Undid last changes

--- model.py
+++ model.py
@@ -14,7 +14,7 @@ class Predictor(nn.Module):
 
     def forward(self, x):
         logits = self.linear_layer(x)
-        predictions = self.output_layer(logits).squeeze(dim=1)
+        predictions = self.output_layer(logits)
 
         return logits, predictions
 
@@ -25,11 +25,11 @@ class Adversary(nn.Module):
 
         self.c = nn.Parameter(torch.ones(1))
 
-        self.w2 = nn.init.xavier_uniform_(nn.Parameter(torch.empty(3, 1)))
+        self.w2 = nn.init.xavier_uniform_(nn.Parameter(torch.empty(3,1)))
         self.b = nn.Parameter(torch.zeros(1))
 
     def forward(self, logits, targets):
         s = torch.sigmoid((1 + torch.abs(self.c)) * logits)
-        z_hat = (torch.cat((s, s*targets, s*(1-targets)),dim=1) @ self.w2 + self.b).squeeze(dim=1)
+        z_hat = (torch.cat((s, s*targets, s*(1-targets)),dim=1) @ self.w2 + self.b)
 
         return z_hat, torch.sigmoid(z_hat)
