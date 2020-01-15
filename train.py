173bb465ceecfa70458e2a827fb20bcf07639c7f
LaMouilleBoucle <leilatalha@gmail.com>
2020-01-15 18:21:57 +0100

    Moved plotting to utils

--- train.py
+++ train.py
@@ -321,40 +321,7 @@ def train():
 
     # plot accuracy and loss curves
     logger.info('Generating plots')
-    fig, axs = plt.subplots(4, 1)
-    axs[0].plot(np.arange(1, args.n_epochs +1), av_train_losses_P, label="Train loss predictor", color="#E74C3C")
-    #axs[0].plot(np.arange(1, args.n_epochs +1), av_val_losses_P, label="Val loss predictor", color="#8E44AD")
-
-    axs[2].plot(np.arange(1, args.n_epochs + 1), train_accuracies_P, label='Train accuracy predictor', color="#229954")
-    #axs[2].plot(np.arange(1, args.n_epochs + 1), val_accuracies_P, label='Val accuracy predictor', color="#E67E22")
-
-    if args.debias:
-        axs[1].plot(np.arange(1, args.n_epochs +1), av_train_losses_A, label="Train loss adversary", color="#3498DB")
-        #axs[1].plot(np.arange(1, args.n_epochs +1), av_val_losses_A, label="Val loss adversary", color="#FFC300")
-
-        axs[3].plot(np.arange(1, args.n_epochs + 1), train_accuracies_A, label='Train accuracy adversary', color="#229954")
-        #axs[3].plot(np.arange(1, args.n_epochs + 1), val_accuracies_A, label='Val accuracy adversary', color="#E67E22")
-
-    axs[0].set_xlabel('Epochs')
-    axs[0].set_ylabel('Loss')
-    axs[0].legend(loc="upper right")
-
-    axs[1].set_xlabel('Epochs')
-    axs[1].set_ylabel('Loss')
-    axs[1].legend(loc="upper right")
-
-    axs[2].set_xlabel('Epochs')
-    axs[2].set_ylabel('Accuracy')
-    axs[2].legend(loc="upper right")
-
-    axs[3].set_xlabel('Epochs')
-    axs[3].set_ylabel('Accuracy')
-    axs[3].legend(loc="upper right")
-
-    plt.tight_layout()
-
-    plt.show()
-
+    utils.plot_loss_acc((av_train_losses_P,train_accuracies_P), (av_train_losses_A,train_accuracies_A))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import torch
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
 
 
@@ -29,6 +30,40 @@ def project_grad(x, v):
     proj_grad = torch.dot(x, norm_v) * v
     return proj_grad
 
+
+def plot_loss_acc(P, A=None):
+    fig, axs = plt.subplots(4, 1)
+    axs[0].plot(np.arange(1, len(P[0])+1), P[0], label="Train loss predictor", color="#E74C3C")
+    #axs[0].plot(np.arange(1, args.n_epochs +1), av_val_losses_P, label="Val loss predictor", color="#8E44AD")
+
+    axs[2].plot(np.arange(1, len(P[1])+1), P[1], label='Train accuracy predictor', color="#229954")
+    #axs[2].plot(np.arange(1, args.n_epochs + 1), val_accuracies_P, label='Val accuracy predictor', color="#E67E22")
+
+    if A is not None:
+        axs[1].plot(np.arange(1, len(A[0])+1), A[0], label="Train loss adversary", color="#3498DB")
+        #axs[1].plot(np.arange(1, args.n_epochs +1), av_val_losses_A, label="Val loss adversary", color="#FFC300")
+
+        axs[3].plot(np.arange(1, len(A[1])+1), A[1], label='Train accuracy adversary', color="#229954")
+        #axs[3].plot(np.arange(1, args.n_epochs + 1), val_accuracies_A, label='Val accuracy adversary', color="#E67E22")
+
+    axs[0].set_xlabel('Epochs')
+    axs[0].set_ylabel('Loss')
+    axs[0].legend(loc="upper right")
+
+    axs[1].set_xlabel('Epochs')
+    axs[1].set_ylabel('Loss')
+    axs[1].legend(loc="upper right")
+
+    axs[2].set_xlabel('Epochs')
+    axs[2].set_ylabel('Accuracy')
+    axs[2].legend(loc="upper right")
+
+    axs[3].set_xlabel('Epochs')
+    axs[3].set_ylabel('Accuracy')
+    axs[3].legend(loc="upper right")
+
+    plt.tight_layout()
+    plt.show()
 
 def calculate_fpr(fp, tn):
     return fp / (fp + tn)
