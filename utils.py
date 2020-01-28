91af3fe26f79ba6d9ec44cf66585dbde5310dad3
Azamat Omuraliev <azamat.omu@gmail.com>
2020-01-28 17:21:28 +0100

    revert roc addition

--- utils.py
+++ utils.py
@@ -174,10 +174,10 @@ def calculate_metrics(true_labels, predictions, true_protected, dataset):
         avg_abs_dif = np.average(np.absolute(protected_differences), axis=1)
         m_prec, m_recall, m_fscore, m_support = precision_recall_fscore_support(true_labels[negative_indices], predictions[negative_indices])
         w_prec, w_recall, w_fscore, w_support = precision_recall_fscore_support(true_labels[positive_indices], predictions[positive_indices])
-        # m_acc = neg_confusion_mat.diagonal()/neg_confusion_mat.sum(axis=1)
-        # w_acc = pos_confusion_mat.diagonal()/pos_confusion_mat.sum(axis=1)
-        m_acc = roc_auc_score(true_labels[negative_indices], predictions[negative_indices])
-        w_acc = roc_auc_score(true_labels[positive_indices], predictions[positive_indices])
+        m_acc = neg_confusion_mat.diagonal()/neg_confusion_mat.sum(axis=1)
+        w_acc = pos_confusion_mat.diagonal()/pos_confusion_mat.sum(axis=1)
+        # m_acc = roc_auc_score(true_labels[negative_indices], predictions[negative_indices])
+        # w_acc = roc_auc_score(true_labels[positive_indices], predictions[positive_indices])
 
         return m_prec, m_recall, m_fscore, m_support, m_acc, w_prec, w_recall, w_fscore, w_support, w_acc, avg_dif, avg_abs_dif
 
