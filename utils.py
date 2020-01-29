f845a1968dde39de47caa454f08566304d7a0722
VanessaBotha <vanessabotha96@gmail.com>
2020-01-29 16:52:24 +0100

    mutual information clean version

--- utils.py
+++ utils.py
@@ -238,30 +238,27 @@ def plot_loss_acc(P, A=None, dataset='adult'):
     plt.savefig(title)
 
 
-
-def entropy(rv1, conditional_rv1 = None, conditional_rv2 = None):
+def entropy(rv1, cond_rv):
     entropy = 0
 
-    if conditional_rv1 is None and conditional_rv2 is None:
-        entropy = 0
+    if cond_rv is None:
+        # Calculate entropy H(rv1)
         distr_rv1 = get_distr(rv1)
         for prob in distr_rv1.values():
             entropy += prob * math.log(1 / prob, 2)
     else: 
-        if conditional_rv1 is not None and conditional_rv2 is not None: 
-            joint = get_joint(conditional_rv1, conditional_rv2)
-            distr_rv1_conditioned = get_conditional_distr(rv1, joint)
-            distr_of_conditional = get_distr(joint)
-        elif conditional_rv1 is not None: 
-            distr_rv1_conditioned = get_conditional_distr(rv1, conditional_rv1)
-            distr_of_conditional = get_distr(conditional_rv1)
+        # Calculate entropy H(rv1 | cond_rv)
+        distr_rv1_conditioned = get_conditional_distr(rv1, cond_rv)
+        distr_cond_rv = get_distr(cond_rv)
 
-        for event, prob in distr_of_conditional.items(): 
+        for event, prob in distr_cond_rv.items(): 
             entropy_part = 0
             for cond_prob in distr_rv1_conditioned[event].values(): 
                 entropy_part += cond_prob * math.log(1 / cond_prob, 2)
             entropy += prob * entropy_part
     return entropy
+
+
 
 def get_joint(rv1, rv2): 
     return [i for i in zip(rv1, rv2)]
@@ -277,7 +274,7 @@ def get_distr(rv):
 
 def get_conditional_distr(rv1, rv2): 
 
-    # Get joint distribution of random variable 1 and 2
+    # Get joint distribution
     distr_joint = get_distr(get_joint(rv1, rv2))
 
     # Get distribution of random variable we want to condition on 
@@ -293,28 +290,27 @@ def get_conditional_distr(rv1, rv2):
 
 
 
-def mutual_information(rv1, rv2, conditional_rv = None):
+def mutual_information(rv1, rv2, cond_rv = None):
 
     mutual_information = None
 
-    if conditional_rv is None: 
+    if cond_rv is None: 
 
         # Compute entropy H(rv1)
         entropy1 = entropy(rv1)
 
         # Compute entropy H(rv1 | rv2)
-        entropy2 = entropy(rv1, conditional_rv1 = rv2)
+        entropy2 = entropy(rv1, cond_rv = rv2)
 
         # Compute mutual information I(rv1; rv2)
         mutual_information = entropy1 - entropy2
 
     else: 
         # Compute entropy H(rv1 | cond_rv)
-        entropy1 = entropy(rv1, conditional_rv1 = conditional_rv)
+        entropy1 = entropy(rv1, cond_rv = cond_rv)
 
         # Compute entropy H(rv1 | cond_rv, rv2)
-        entropy2 = entropy(rv1, conditional_rv1 = conditional_rv, 
-                                            conditional_rv2 = rv2)
+        entropy2 = entropy(rv1, cond_rv = get_joint(cond_rv, rv2))
 
         # # Compute mutual information I(rv1; rv2 | cond_rv)
         mutual_information = entropy1 - entropy2
